a8107559ca6794beb18e53e0ca1518e9ec10caa5
olugovoy <olugovoy@gmail.com>
2021-02-03 10:56:38 -0500

    vignettes updates

--- R/solar_pv_models.R
+++ R/solar_pv_models.R
@@ -1,5 +1,5 @@
 .pv_array_type <- function(array.type) {
-  .tracking_types <- c("fh", "fl", "th", "tl", "tv", "td")
+  .tracking_types <- c("fh", "fl", "th", "tv", "td")
   # .tracking_types <- c("fh", "fl", "th", "tl", "tv", "td")
   # "fh" # "Horizontal (h) fixed (f) arrays"
   # "fl" # "Tilted (l) fixed (f) arrays"
@@ -162,20 +162,25 @@ pv_array_position <- function(x,
     )
     if (i == "fh") { 
       # fixed horizontal ####
-      y$array.azimuth <- 0 # facing equator
+      y$array.azimuth <- 0 # Southern hemisphere facing North
+      y$array.azimuth[x[[lat]] > 0] <- 180 # Northern hemisphere facing South
+      # y$array.azimuth <- 0 # facing equator
       y$array.tilt <- 0
       # y$array.tilt[y$array.tilt < array.tilt.range.fh[1]] <- array.tilt.range.fh[1]
       # y$array.tilt[y$array.tilt > array.tilt.range.fh[2]] <- array.tilt.range.fh[2]
       
     } else if (i == "fl") { 
       # fixed tilted ####
-      y$array.azimuth <- 0 # facing equator
+      y$array.azimuth <- 0 # Southern hemisphere facing North
+      y$array.azimuth[x[[lat]] > 0] <- 180 # Northern hemisphere facing South
+      # y$array.azimuth <- 0 # facing equator
       y$array.tilt <- abs(x[[lat]])
       # y$array.tilt[y$array.tilt < array.tilt.range.fl[1]] <- array.tilt.range.fl[1]
       # y$array.tilt[y$array.tilt > array.tilt.range.fl[2]] <- array.tilt.range.fl[2]
     } else if (i == "th") { 
       # tracking horizontal ####
-      y$array.azimuth <- -90 + as.numeric(x[[azimuth]] >= 0) * 180
+      y$array.azimuth <- 90 + as.numeric(x[[azimuth]] > 180) * 180
+      # y$array.azimuth <- -90 + as.numeric(x[[azimuth]] >= 0) * 180
       # ii <- x[[zenith]] < 90
       y$array.tilt[ii] <- 
         atan(abs(
@@ -216,6 +221,8 @@ pv_array_position <- function(x,
       # tracking tilted ####
       # browser()
       y$array.tilt <- abs(x[[lat]])
+      y$array.tilt[y$array.tilt < tilt.param$tl$min] <- tilt.param$tl$min
+      y$array.tilt[y$array.tilt > tilt.param$tl$max] <- tilt.param$tl$max
       y$array.azimuth <- 0 # facing equator
       y[[zenith]] <- as.numeric(NA)
       y[[azimuth]] <- as.numeric(NA)
@@ -230,20 +237,25 @@ pv_array_position <- function(x,
       )
       
       delta.gamma <- atan(
-        sind(y[[zenith]][ii]) * sind((y[[azimuth]][ii] - 0)) /
+        # sind(y[[zenith]][ii]) * sind((y[[azimuth]][ii] - 0)) /
+        sind(y[[zenith]][ii]) * sind((y[[azimuth]][ii] - 180)) /
           (cos(AOI.fl) * sind(y$array.tilt[ii]))
         ) / pi * 180
       # rm(array.azimuth,array.tilt,AOI.fx)
       
       y$array.azimuth[ii] <- 
-        (delta.gamma + ((delta.gamma * y[[azimuth]][ii]) < 0) * 
-           (2*(y[[azimuth]][ii] >= 0) - 1) * 180) #* (zenith < 90)
+        (180 + delta.gamma + ((delta.gamma * (y[[azimuth]][ii] - 180)) < 0) * 
+           (2*((y[[azimuth]][ii] - 180) >= 0) - 1) * 180) #* (zenith < 90)        
+        # (delta.gamma + ((delta.gamma * y[[azimuth]][ii]) < 0) * 
+        #    (2*(y[[azimuth]][ii] >= 0) - 1) * 180) #* (zenith < 90)
       rm(delta.gamma, AOI.fl)
       # gc()
       y$array.tilt[ii] <- (
         atan(
-          tand(y$array.tilt[ii]) / cosd(y$array.azimuth[ii])) + 
-          (cosd(y$array.azimuth[ii]) < 0) * pi) / pi * 180 
+          tand(y[[zenith]][ii]) / cosd((y[[azimuth]][ii] - 180))) + 
+          (cosd(y[[azimuth]][ii] - 180) < 0) * pi) / pi * 180           
+          # tand(y$array.tilt[ii]) / cosd(y$array.azimuth[ii])) + 
+          # (cosd(y$array.azimuth[ii]) < 0) * pi) / pi * 180 
       y$array.tilt[!ii] <- 0
       
       y$zenith <- NULL; y$azimuth <- NULL
@@ -318,7 +330,7 @@ tilt.param.default <- function(x = NULL) {
     fh = list(min = 0, max = 0, shading = 90, backtracking = FALSE),
     fl = list(min = 0, max = 75, shading = 90, backtracking = FALSE),
     th = list(min = 0, max = 90, shading = 90, backtracking = TRUE),
-    tl = list(min = 0, max = 90, shading = 90, backtracking = TRUE),
+    tl = list(min = 0, max = 89, shading = 90, backtracking = TRUE),
     tv = list(min = 0, max = 75, shading = 90, backtracking = FALSE),
     td = list(min = 0, max = 60, shading = 90, backtracking = TRUE)
   )
@@ -601,7 +613,8 @@ sind <- function(x, check = TRUE) {
   return(x)
 }
 
-tand <- function(x) {
+tand <- function(x, Inf.eps = 1e-12, Inf.val = NaN) {
+  
   tanpi(x / 180) # same speed as tanpi(x / 180) & same divergence from tan
 }
 
--- README.md
+++ README.md
@@ -5,14 +5,21 @@ Tools to process MERRA-2 data (NASA) with focus on renewable energy
 
 (UNDER CONSTRUCTION)
 
-Estimated annual wind power output at 50 meters height, 40-years average
+``` r
+fig_file <- "images/ghi_40y_avr_24steps.png"
+include_graphics(fig_file)
+```
+
+<img src="images/ghi_40y_avr_24steps.png" width="2550" />
 
 <div class="figure">
 
-<img src="images/poa_fl_40y_avr_24steps.png" alt="Estimated annual solar power output (fixed, optimal tilt angle), 40-years average" width="2850" />
+<img src="images/poa_fl_40y_avr_24steps.png" alt="Estimated annual solar power output (fixed, optimal tilt angle), 40-years average" width="2550" />
 <p class="caption">
 Estimated annual solar power output (fixed, optimal tilt angle),
 40-years average
 </p>
 
 </div>
+
+Estimated annual wind power output at 50 meters height, 40-years average
